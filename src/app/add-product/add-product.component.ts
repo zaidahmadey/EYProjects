0c0d66d20814bb79b14d280546a9e0e97da7e404
Zaid Ahmad <zaidahmadey@gmail.com>
2019-05-10 01:48:30 +0530

    Api Code Updated

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AddProductModel} from '../models/addproduct.model';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  styleUrls: ['./add-product.component.css'],
+  providers:[AddProductModel],
 })
 export class AddProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(public addProductModelObj:AddProductModel) { }
 
   ngOnInit() {
   }
-
+SaveData(){
+  var test=this.addProductModelObj.productName;
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { HomeComponent } from './home/home.component';
-
+import {HttpClient} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule,HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
